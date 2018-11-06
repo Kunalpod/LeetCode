8e3446f961bf5625472a901f68d6c4bd4f82843b
Kunal Gautam <Kunalpod@users.noreply.github.com>
2018-11-06 09:36:19 -0800

    Create binary_gap.py
    Problem Description: https://leetcode.com/problems/binary-gap/description/

---
+++ binary_gap.py
@@ -0,0 +1,10 @@
+def binaryGap(self, N):
+    count = 0
+    nB = bin(N)[2:]
+    nBL = len(nB)
+    j = 0   #Last recorded instance of '1'        
+    for i in range(1, nBL):
+        if nB[i] == '1': 
+            count= max(count, i - j)
+            j = i
+    return count
